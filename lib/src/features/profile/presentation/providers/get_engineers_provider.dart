9ab3844a9170194588f0186c5772a192ddfbc71e
coding-faizan <faizanahmad8292@gmail.com>
2025-09-05 15:05:14 +0500

    error fix

--- lib/src/features/profile/presentation/providers/get_engineers_provider.dart
+++ lib/src/features/profile/presentation/providers/get_engineers_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jira_clone/src/features/auth/presentation/providers/auth_state_controller.dart';
-import 'package:jira_clone/src/features/profile/data/engineer_repo.dart';
+import 'package:jira_clone/src/features/profile/data/engineer/engineer_repo.dart';
 import 'package:jira_clone/src/features/profile/domain/engineer.dart';
 
 final getEngineersProvider = FutureProvider<List<Engineer>>((ref) async {
